873b8bcd76fb6d4f89bbde20ab7d8eeea6e437f0
andrija.stevanovic <andrija.stevanovic@elfak.rs>
2025-08-05 13:12:04 +0200

    voznja overview osnova

---
+++ client/web_app/src/components/models/voznja/VoznjaOverview.tsx
@@ -0,0 +1,13 @@
+import type {Voznja} from "../../../types.ts";
+
+interface Props{
+  voznja: Voznja
+}
+
+function VoznjaOverview({voznja}: Props) {
+  return (
+    <div></div>
+  );
+}
+
+export default VoznjaOverview;
